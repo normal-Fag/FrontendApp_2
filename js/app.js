c7c53f9047ce0bf8040ef8d8f90202e73d67a462
normal-Fag <72517164+normal-Fag@users.noreply.github.com>
2021-10-03 00:57:48 +0700

    Welcome section almost complete

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 console.log("Hello")
+$(document).ready(function(){
+	$('.header__burger').click(function(event){
+		$('.header__burger, .menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	})
+})
