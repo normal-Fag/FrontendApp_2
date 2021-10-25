e2e4533b5f6e8babe6ffdfbadee9e9520c36047c
normal-Fag <72517164+normal-Fag@users.noreply.github.com>
2021-10-25 11:42:24 +0700

    modified app.js

--- js/app.js
+++
@@ -1,7 +0,0 @@
-const burger = document.querySelector('.header__burger');
-const menu  = document.querySelector('.menu');
-
-burger.addEventListener('click', function() {
-	menu.classList.toggle('active');
-	burger.classList.toggle('active');
-})
