9d0f89fbe8bcef3b0999305a5ef7af61048e9a28
normal-Fag <72517164+normal-Fag@users.noreply.github.com>
2021-10-25 19:43:18 +0700

    _None

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// <<<<<<< HEAD
+// =======
+const burger = document.querySelector('.header__burger');
+const menu  = document.querySelector('.menu');
+
+burger.addEventListener('click', function() {
+	menu.classList.toggle('active');
+	burger.classList.toggle('active');
+})
+// >>>>>>> parent of d9fc2de (Merge pull request #2 from normal-Fag/feat-sightseens)
